ed56b9194da897308d11a3578d81652dd4e65ff0
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-12-02 23:27:50 -0600

    more changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,12 +15,44 @@ let connection = mysql.createConnection({
   password: process.env.MYSQL,
   database: "bamazon_DB"
 });
-console.log(connection);
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   con.query("SELECT * FROM customers", function(err, result, fields) {
-//     if (err) throw err;
-//     console.log(result);
-//   });
-// });
+connection.connect(function(err) {
+  if (err) throw err;
+  connection.query("SELECT *  FROM products", function(err, result) {
+    if (err) throw err;
+    console.log(result);
+
+    inquirer
+      .prompt([
+        {
+          name: "chooseProduct",
+          type: "input",
+          message: "What can I do 'ya for? Don't be shy, enter the product ID."
+        },
+        {
+          name: "chooseQuantity",
+          type: "input",
+          message: "How many 'ya need?"
+        }
+      ])
+      .then(function(answers) {
+        //if answers.chooseproduct != a product, tell them.
+        //if answers.chooseproduct does = a product, compare choosequantity to stock_quantity
+        let chosenItem = "";
+        for (let i = 0; i < result.length; i++) {
+          if (result[i].item_name === answers.chooseProduct) {
+            console.log("logic worked " + answers.chooseProduct);
+            chosenItem = result[i];
+          }
+        }
+
+        // if (answers.chooseProduct > 9) {
+        //   console.log("We don't seem to have that in stock, come again!");
+        // } else if (answers.chooseProduct > result[9].stock_quantity) {
+        //   console.log(
+        //     "Seems as though we don't have enough for ya! Come back again soon!"
+        //   );
+        // }
+      });
+  });
+});
